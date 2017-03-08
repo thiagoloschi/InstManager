f6a40896f74b6a8947e564f5afb7b787f4df02bc
Thiago Loschi <thiagoloschi@gmail.com>
2017-03-08 03:51:21 -0300

    Adding the main page

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -3,4 +3,10 @@ var instApp = angular.module('instApp');
 instApp.controller('mainController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
 	console.log('mainController loaded...');
 
+	$scope.getUser = function(){
+		$http.get('/data/user').then(function(response) {
+			$scope.user = response.data;
+		})
+	}
+
 }]);
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//'use strict';
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -20,7 +21,7 @@ exports.authorize_user = function(req, res){
 };
 
 exports.handleauth = function(req, res) {
-	console.log('/call');	
+	console.log('/call');
 	ig.authorize_user(req.query.code, api.redirect_uri, function(err, result) {
 		if (err) {
 		  console.log(err.body);
@@ -28,20 +29,20 @@ exports.handleauth = function(req, res) {
 		} else {
 		  console.log('Access token is ' + result.access_token);
 		  user = result.user;
-		  console.log(user);
 		  res.redirect('http://localhost:5000/#!/main');
 		}
 	});
 };
 
+//------------------------ ROUTES --------------------------
 app.get('/', function(req, res) {
 	res.send('Go to /login');
 });
-
 app.get('/login', exports.authorize_user);
-
-
 app.get('/callback/', exports.handleauth);
+app.get('/data/user', function(req, res) {
+		res.send(user);
+});
 
 app.listen(5000);
-console.log('InstManager is now running...'); 
+console.log('InstManager is now running...');
